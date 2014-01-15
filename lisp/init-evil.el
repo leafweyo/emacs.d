b004374fa2ac873549f11b6cf1a3cf62d52b1426
leafweyo <leafweyo@gmail.com>
2014-01-16 00:30:34 +0800

    enhance

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -3,6 +3,8 @@
 (global-evil-leader-mode)
 (evil-leader/set-leader ";")
 (require-package 'key-chord)
+(setq key-chord-two-keys-delay 1)
+(setq key-chord-one-keys-delay 1)
 (key-chord-mode 1)
 (require-package 'evil-nerd-commenter)
 (evilnc-default-hotkeys)
@@ -24,6 +26,7 @@
 (define-key evil-normal-state-map (kbd "SPC") 'evil-ex)
 ;;(define-key evil-insert-state-map )
 (key-chord-define evil-insert-state-map ";f" (lambda () (interactive) (progn (save-buffer) (evil-normal-state))))
+(key-chord-define evil-insert-state-map ";s" 'switch-window)
 (evil-leader/set-key "f" 'save-buffer)
 (evil-leader/set-key
   ;; "b" 'switch-to-buffer
@@ -40,9 +43,9 @@
   "im" 'imenu
   "kw" 'delete-window
   "ko" 'delete-other-windows
-  "ss" 'switch-window
-  "sl" 'split-window-right
-  "sj" 'split-window-below
+  "s" 'switch-window
+  ;; "sl" 'split-window-right
+  ;; "sj" 'split-window-below
   )
 (global-set-key (kbd "C-;") 'smex)
 (defalias 'list-buffers 'ibuffer)
---
+++ lisp/init-ui.el
@@ -0,0 +1,3 @@
+(require-package 'project-explorer)
+(require 'project-explorer)
+(provide 'init-ui)
